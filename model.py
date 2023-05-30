d9367396e0eccbb809756aaf5519d485cf6b98e4
Maksim <ivanmax556@gmail.com>
2023-05-30 19:52:28 +0400

    Created model

--- model.py
+++ model.py
@@ -1,1 +1,11 @@
+def data(): # получаем из файла
+  with open('data_file.txt', 'r', encoding='utf-8') as data_file:
+    data = data_file.read().split('\n')
 
+  return data
+
+def add_contact(contact): # добавляем контакт
+  with open('data_file.txt', 'a', encoding='utf-8') as data_file:
+    data_file.write(f'{contact}\n')
+
+  return contact
